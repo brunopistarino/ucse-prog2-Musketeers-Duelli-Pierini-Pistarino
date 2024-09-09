2187f6c301d92c0d94469e0a8e249dd8ddd6dadf
Luca-Pierini-GitH <lucapierini03@gmail.com>
2024-09-09 17:22:41 -0300

    Fechas de modificacion y creacion agregadas, creacion model de Receta

--- api/model/Compra.go
+++ api/model/Compra.go
@@ -5,5 +5,6 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Compra struct {
 	ID         primitive.ObjectID `bson:"_id,omitempty"` //omitempty agrega el campo solo si no es nil
 	CostoTotal float32            `bson:"costo_total"`
-	Fecha      primitive.DateTime `bson:"fecha"`
+	FechaCreacion      primitive.DateTime `bson:"fechaCreacion"`
+	FechaActualizacion primitive.DateTime `bson:"fechaActualizacion"`
 }
