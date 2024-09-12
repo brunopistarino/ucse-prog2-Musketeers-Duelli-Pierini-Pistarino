038ba868f1b72d7904ed109c0faeeaef5380d98e
Bruno Pistarino <brunopistarino@gmail.com>
2024-09-12 11:35:53 -0300

    Fix navegación

--- web/app/(dashboard)/layout.tsx
+++ web/app/(dashboard)/layout.tsx
@@ -31,7 +31,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const currentPage = "/compras";
+  // const currentPage = "/compras";
+  const currentPage = "/TODO";
 
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] bg-muted/40">
