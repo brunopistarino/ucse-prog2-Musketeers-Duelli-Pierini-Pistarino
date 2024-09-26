776a6274d216bcd49372935a6b50d47f866eed9a
Bruno Pistarino <brunopistarino@gmail.com>
2024-09-26 16:40:33 -0300

    Add recipes form

--- web/app/(dashboard)/recetas/page.tsx
+++ web/app/(dashboard)/recetas/page.tsx
@@ -3,11 +3,19 @@ import ModeToggle from "@/components/mode-toggle";
 import Filters from "./filters";
 import { getRecipes } from "@/lib/actions/recipes";
 import ErrorPage from "@/components/error-page";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import FormDialog from "./form-dialog";
+import { getFoodstuffs } from "@/lib/actions/foodstuffs";
 
 export default async function Page() {
-  const { data, error } = await getRecipes();
+  const [
+    { data: recipes, error: recipesError },
+    { data: foodstuffs, error: foodstuffsError },
+  ] = await Promise.all([getRecipes(), getFoodstuffs()]);
 
-  if (error || !data) {
+  if (recipesError || !recipes || foodstuffsError || !foodstuffs) {
+    const error = recipesError || foodstuffsError || "";
     return <ErrorPage error={error} />;
   }
 
@@ -21,9 +29,15 @@ export default async function Page() {
             <DollarSign size={16} />
             Hacer compra
           </Button> */}
+          <FormDialog foodstuffs={foodstuffs}>
+            <Button className="gap-2">
+              <Plus size={16} />
+              Agregar receta
+            </Button>
+          </FormDialog>
         </div>
       </div>
-      <MasonryRecipes recipes={data} />
+      <MasonryRecipes recipes={recipes} />
     </div>
   );
 }
